0dd3a6bf7a2802f5e29dad6cbfe4935e0c6298f8
Sterne Lee <sternelee@gmail.com>
2020-07-02 18:06:23 +0800

    更新

--- src/controller/post-controller.ts
+++ src/controller/post-controller.ts
@@ -1,10 +1,6 @@
 import * as ioredis from "ioredis";
 import * as shortid from "shortid";
-// import * as redis from "redis";
-// import { promisify } from "util";
 
-// const io = redis.createClient()
-// const client = promisify(io).bind(redis);
 const redis = new ioredis({
   port: 6379, // Redis port
   host: "localhost", // Redis host
@@ -13,13 +9,14 @@ const redis = new ioredis({
 
 export default class PostController {
   static async get(ctx) {
-    const { uid = "master" } = ctx.body.request;
+    const { uid = "master" } = ctx.request.query;
     const keys = await redis.keys(uid + "--*");
-    const datas = redis.mget(keys);
-    ctx.body = datas;
+    if (keys.length === 0) return ctx.body = []
+    const datas = await redis.mget(keys);
+    ctx.body = datas.reverse();
   }
   static async post(ctx) {
-    const { uid = "master", val = "" } = ctx.body.request;
+    const { uid = "master", val = "" } = ctx.request.body;
     const uuid = await shortid.generate();
     await redis.set(uid + "--" + uuid, val, "EX", 60 * 60);
     ctx.body = "ok";
